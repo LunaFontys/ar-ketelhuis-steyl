9cf76fdeaec4ba2e6bb4ebb46392f484ff058145
LunaFontys <532952@student.fontys.nl>
2026-04-08 22:49:24 +0200

    Marker based tacking implemented

---
+++ TestBuild9_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,78 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void PlaceOnPlane_Update_m4BAC7B0437CB4CB61EF9C23849AC096E5EE7C2B7 (void);
+extern void PlaceOnPlane_HideAllPlanes_mDF55697DA651F1BA5785382265FB7389D25E2302 (void);
+extern void PlaceOnPlane__ctor_mEAC8D0276C9BC36A8A633F162E3F8EBF96D6255C (void);
+extern void PlaceOnPlane__cctor_m5A9F98972E150B888F0006CF7083FCC92CD21F39 (void);
+extern void WheelDragRotate_Start_mA36094331C04DC046C4C514103D4ABDCC1E7318A (void);
+extern void WheelDragRotate_Update_m2C7948362DC814182183F70D8061E12E46C95068 (void);
+extern void WheelDragRotate_CheckSteamStop_m3FB5455DF4A1D399A15CDF4A181BCEBB1D0547A0 (void);
+extern void WheelDragRotate__ctor_mF752DE7BD6FCAE566EF853E9DC61809438DE717C (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+extern void PipeShaker_Start_mF840AFB16D007BA5AD47AF397B0B44C2D59D3155 (void);
+extern void PipeShaker_Update_mB7225466B88EB8149076C66C00FFB0F3B50218E5 (void);
+extern void PipeShaker_SetShaking_m4995771BF085F1FF1C408F64FB9EFC4DC684B5F0 (void);
+extern void PipeShaker__ctor_m3A28CEB24396221FF2AD05835856E5939F9A6FEC (void);
+static Il2CppMethodPointer s_methodPointers[14] = 
+{
+	PlaceOnPlane_Update_m4BAC7B0437CB4CB61EF9C23849AC096E5EE7C2B7,
+	PlaceOnPlane_HideAllPlanes_mDF55697DA651F1BA5785382265FB7389D25E2302,
+	PlaceOnPlane__ctor_mEAC8D0276C9BC36A8A633F162E3F8EBF96D6255C,
+	PlaceOnPlane__cctor_m5A9F98972E150B888F0006CF7083FCC92CD21F39,
+	WheelDragRotate_Start_mA36094331C04DC046C4C514103D4ABDCC1E7318A,
+	WheelDragRotate_Update_m2C7948362DC814182183F70D8061E12E46C95068,
+	WheelDragRotate_CheckSteamStop_m3FB5455DF4A1D399A15CDF4A181BCEBB1D0547A0,
+	WheelDragRotate__ctor_mF752DE7BD6FCAE566EF853E9DC61809438DE717C,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+	PipeShaker_Start_mF840AFB16D007BA5AD47AF397B0B44C2D59D3155,
+	PipeShaker_Update_mB7225466B88EB8149076C66C00FFB0F3B50218E5,
+	PipeShaker_SetShaking_m4995771BF085F1FF1C408F64FB9EFC4DC684B5F0,
+	PipeShaker__ctor_m3A28CEB24396221FF2AD05835856E5939F9A6FEC,
+};
+static const int32_t s_InvokerIndices[14] = 
+{
+	10576,
+	10576,
+	10576,
+	15657,
+	10576,
+	10576,
+	10576,
+	10576,
+	15687,
+	10576,
+	10576,
+	10576,
+	8199,
+	10576,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	14,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
